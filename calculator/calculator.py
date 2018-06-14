753ec55db410172b30d42d03884cde300adb5a18
Pawel W <theYoursTruly@users.noreply.github.com>
2018-06-14 21:11:44 +0200

    Calculator - Finished interface

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -8,10 +8,43 @@
 import math
 from appJar import gui
 
+# GLOBALS
+mem = 0     # memory
 
-""" UI looks like this:
+# Set general app configuration
+def configure_gui():
+    app.setBg("#333333", override=False)
+    app.setPadding([1,1])
+    app.setInPadding([7,3])
+    app.setFont(size=20, family="Verdana")
+    app.setButtonFont(size=14, family="Consolas")
+    app.setStretch("both")
+    app.setSticky("nesw")
+
+# Assign actions for keys for keyboard control
+def set_keybinds():
+    for button in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "/",
+                   "*", "+", "-", "(", ")", "<BackSpace>", "<Return>", "<Escape>"]:
+        app.bindKey(button, key_press)
+
+# React to a keypress
+def key_press(key):
+    if key == "<BackSpace>":
+        undo("")
+    elif key == "<Return>":
+        calculate("")
+    elif key == "<Escape>":
+        exit(0)
+    elif key in ["1", "2", "3", "4", "5", "6", "7", "8", "9",
+                      "0", ".", "/", "*", "+", "-", "(", ")"]:
+        push(key)
+
+""" Create a gui (buttons and such)
+
+UI looks like this:
+[                       operation ]
 [                          result ]
-[ MC][ M+][ M-][ ( ][ ) ][ % ][ : ]
+[ MC][ MR][ M+][ ( ][ ) ][ % ][ : ]
 [sin][cos][tan][ 7 ][ 8 ][ 9 ][ x ]
 [x^y][ √ ][mod][ 4 ][ 5 ][ 6 ][ - ]
 [log][ ln][ x!][ 1 ][ 2 ][ 3 ][ + ]
@@ -19,45 +52,61 @@ from appJar import gui
 """
 def draw_ui():
     # row 0
-    app.addSelectableLabel("result", "0", 0, 0, 7)
+    app.addLabel("operation", "", 0, 0, 7)
+    app.setLabelAlign("operation", "right")
+    app.setLabelFg("operation", "grey")
     # row 1
-    for idx, elem in enumerate([" MC", " M+",  " M-"]):
-        app.addButton(elem, memory, 1, idx)
-    for idx, elem in enumerate([" ( ", " ) ", " % ", " : "], 3):
-        app.addButton(elem, operation, 1, idx)
+    app.addNumericEntry("result", 1, 0, 7)
+    app.setEntry("result", "0", callFunction=True)
+    app.setEntryAlign("result", "right")
     # row 2
-    for idx, elem in enumerate(["sin", "cos", "tan", " 7 ", " 8 ", " 9 ", " x "]):
-        app.addButton(elem, operation, 2, idx)
+    for idx, elem in enumerate([" MC", " MR",  " M+"]):
+        app.addButton(elem, memory, 2, idx)
+    for idx, elem in enumerate([" ( ", " ) ", " % ", " : "], 3):
+        app.addButton(elem, push, 2, idx)
     # row 3
-    for idx, elem in enumerate([" x\u207F", " √ ", "mod", " 4 ", " 5 ", " 6 ", " - "]):
-        app.addButton(elem, operation, 3, idx)
+    for idx, elem in enumerate(["sin", "cos", "tan", " 7 ", " 8 ", " 9 ", " x "]):
+        app.addButton(elem, push, 3, idx)
     # row 4
-    for idx, elem in enumerate(["log", " ln", " a!", " 1 ", " 2 ", " 3 ", " + "]):
-        app.addButton(elem, operation, 4, idx)
+    for idx, elem in enumerate([" x\u207F", " √ ", "mod", " 4 ", " 5 ", " 6 ", " - "]):
+        app.addButton(elem, push, 4, idx)
     # row 5
-    for idx, elem in enumerate([" e ", " π ", " . ", " C ", " 0 "]):
-        app.addButton(elem, operation, 5, idx)
-    app.addButton("       = ", calculate, 5, 5, 2)
+    for idx, elem in enumerate(["log", " ln", " a!", " 1 ", " 2 ", " 3 ", " + "]):
+        app.addButton(elem, push, 5, idx)
+    # row 6
+    for idx, elem in enumerate([" e ", " π ", " . "]):
+        app.addButton(elem, push, 6, idx)
+    app.addButton(" C ", undo, 6, 3)
+    app.addButton(" 0 ", push, 6, 4)
+    app.addButton("       = ", calculate, 6, 5, 2)
 
-def operation(button):
-    pass    # TODO
+# Add given character to the current calculation line
+def push(button):
+    app.setLabel("operation", app.getLabel("operation") + button.strip())
 
+# Manage calculator memory
 def memory(button):
-    pass    # TODO
+    global mem
+    if button.strip() ==  "MC":
+        mem = 0
+    if button.strip() ==  "MR":
+        app.setEntry("result", mem)
+    if button.strip() ==  "M+":
+        mem += app.getEntry("result")
 
+# Remove last character from the current calculation line
+def undo(button):
+    operation = app.getLabel("operation")
+    if len(operation) > 0:
+        operation = operation[:-1]
+        app.setLabel("operation", operation)
+
+# Make an actual calculation
 def calculate(button):
-    pass    # TODO
-
+    pass #TODO
 
 if __name__ == "__main__":
-    app = gui("Calculator")
-    app.setIcon("appJar/resources/icons/calculator-alt.png")
-    app.setBg("white", override=False)
-    app.setPadding([1,1])
-    app.setInPadding([7,3])
-    app.setFont(size=20, family="Verdana")
-    app.setButtonFont(size=14, family="Consolas")
-
-    draw_ui()
-
-    app.go()
+    with gui("Calculator") as app:
+        configure_gui()
+        set_keybinds()
+        draw_ui()
